9fa7ee22e173e27653d27cffea6d9a3a5a853744
Pine <rajiohousou@gmail.com>
2023-06-14 18:22:17 +0900

    add measurement time

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <random>
-#include <time.h>
+#include <chrono>
 
-const int LOOP_TIME = 900000;
+const int LOOP_TIME = 9000000;
 const int RADIUS = 100;
 
 int main()
 {
+    std::chrono::system_clock::time_point loopStart, loopEnd;
+
     int insidePoints = 0;
     int outsidePoints = 0;
+
+    loopStart = std::chrono::system_clock::now();
 
     for (int i = 0; i < LOOP_TIME; i++)
     {
@@ -25,9 +29,16 @@ int main()
         }
     }
 
+    loopEnd = std::chrono::system_clock::now();
+
+    double loopElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(loopEnd - loopStart).count();
+
     double sum = insidePoints + outsidePoints;
     double calculatedPI = (4.0 * insidePoints) / sum;
+
     std::cout << calculatedPI << std::endl;
+    std::cout << "Looptime: " <<LOOP_TIME << std::endl;
+    std::cout << "Time: " << loopElapsed / 1000 << "s" << std::endl;
 
     return 0;
 }
