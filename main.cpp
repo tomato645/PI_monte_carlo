c9fcadd767b398c613f2a4f667ef5a3298ef61b8
Pine <rajiohousou@gmail.com>
2023-06-17 23:18:57 +0900

    optimizing calculation

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <chrono>
 #include <ctime>
 
-long long int LOOP_TIME = 1000*1000*1000;
-const int RADIUS = 100;
+long long int LOOP_TIME = 1000 * 1000 * 1000;
+const int RADIUS = 1;
 
 int main(int argc, char *argv[])
 {
@@ -15,38 +15,38 @@ int main(int argc, char *argv[])
 
     std::chrono::system_clock::time_point loopStart, loopEnd;
 
-    int insidePoints = 0;
-    int outsidePoints = 0;
+    double insidePoints = 0;
+    // int outsidePoints = 0;
 
     loopStart = std::chrono::system_clock::now();
 
-    char progress[10];
     for (long long int i = 0; i < LOOP_TIME; i++)
     {
         srand(time(NULL));
         int x = rand() % RADIUS;
         int y = rand() % RADIUS;
 
-        if (x * x + y * y > RADIUS * RADIUS)
-        {
-            outsidePoints++;
-        }
-        else if (x * x + y * y <= RADIUS * RADIUS)
+        // if (x * x + y * y > RADIUS * RADIUS)
+        // {
+        //     outsidePoints++;
+        // }
+        if (x * x + y * y <= RADIUS * RADIUS)
         {
             insidePoints++;
         }
 
-        if (i % (LOOP_TIME / 10) == 0 && i != 0)
-        {
-            std::cout << progress << "実行中" << std::endl;
-        }
+        // if (i % (LOOP_TIME / 10) == 0 && i != 0)
+        // {
+        //     std::cout << progress << "実行中" << std::endl;
+        // }
     }
 
     loopEnd = std::chrono::system_clock::now();
 
     double loopElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(loopEnd - loopStart).count();
 
-    double sum = insidePoints + outsidePoints;
+    // double sum = insidePoints + outsidePoints;
+    double sum = LOOP_TIME;
     double calculatedPI = (4.0 * insidePoints) / sum;
 
     std::cout << calculatedPI << std::endl;
