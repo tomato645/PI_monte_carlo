d2efe112882179ef2536c5562760ed91d0c0e4dd
Pine <rajiohousou@gmail.com>
2023-06-24 23:04:39 +0900

    all renewal

--- main.cpp
+++ main.cpp
@@ -1,57 +1,69 @@
 #include <iostream>
 #include <random>
-#include <chrono>
-#include <ctime>
+#include <string>
 
-long long int LOOP_TIME = 1000 * 1000;
-const int RADIUS = 1;
+// int LOOP_TIME = 10;
+long insideCount = 0;
+
+long double makeRand()
+{
+    // 乱数生成器を初期化
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<long double> dis(0.0, 1.0);
+
+    // 0から1までの乱数を生成して返す
+    return dis(gen);
+}
 
 int main(int argc, char *argv[])
 {
-    if (argv[1] != NULL)
+    int LOOP_TIME;
+    if (argc >= 2)
     {
         LOOP_TIME = atoi(argv[1]);
     }
-
-    std::chrono::system_clock::time_point loopStart, loopEnd;
-
-    double insidePoints = 0;
-    // int outsidePoints = 0;
-
-    loopStart = std::chrono::system_clock::now();
-
-    for (long long int i = 0; i < LOOP_TIME; i++)
+    else
     {
-        srand(time(NULL));
-        int x = rand() % RADIUS;
-        int y = rand() % RADIUS;
-
-        // if (x * x + y * y > RADIUS * RADIUS)
-        // {
-        //     outsidePoints++;
-        // }
-        if (x * x + y * y <= RADIUS * RADIUS)
-        {
-            insidePoints++;
-        }
-
-        // if (i % (LOOP_TIME / 10) == 0 && i != 0)
-        // {
-        //     std::cout << progress << "実行中" << std::endl;
-        // }
+        LOOP_TIME = 1000 * 100;
+        // printf("引数が足りません\n");
+        // exit(1);
     }
 
-    loopEnd = std::chrono::system_clock::now();
+    std::cout << LOOP_TIME << "回実行します。" << std::endl;
 
-    double loopElapsed = std::chrono::duration_cast<std::chrono::milliseconds>(loopEnd - loopStart).count();
+    long double x[LOOP_TIME];
+    long double y[LOOP_TIME];
 
-    // double sum = insidePoints + outsidePoints;
-    double sum = LOOP_TIME;
-    double calculatedPI = (4.0 * insidePoints) / sum;
+    for (int i = 0; i < LOOP_TIME; i++)
+    {
+        x[i] = makeRand();
+        y[i] = makeRand();
+        // std::cout << "x:" << x[i] << " y:" << y[i] << std::endl;
+        if (i % (LOOP_TIME / 10) == 0)
+        {
+            char test[] = "123456789";
+            std::cout << "\r" << std::flush;
+            for (int j = 0; j < i / (LOOP_TIME / 10); j++)
+            {
+                std::cout << test[j] << std::flush;
+            }
+        }
+    }
+    std::cout << std::endl;
+    std::cout << "配列の生成が終了" << std::endl;
 
-    std::cout << calculatedPI << std::endl;
-    std::cout << "Looptime: " << LOOP_TIME << std::endl;
-    std::cout << "Time: " << loopElapsed / 1000 << "s" << std::endl;
+    for (int i = 0; i < LOOP_TIME; i++)
+    {
+        long double powed = (x[i] * x[i]) + (y[i] * y[i]);
+        // std::cout << powed << std::endl;
+        if (powed <= 1.0)
+        {
+            insideCount++;
+        }
+    }
 
-    return 0;
+    std::cout << "insideCount = " << insideCount << std::endl;
+    long double tmp = (4 * insideCount);
+    std::cout << "PI = " << tmp / (LOOP_TIME) << std::endl;
 }
